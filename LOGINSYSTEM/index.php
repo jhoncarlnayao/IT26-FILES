cc5b3e85272bec08263f4484d8625447a0405da2
Fuujin <133795177+jhoncarlnayao@users.noreply.github.com>
2024-02-08 04:42:33 +0800

    Add files via upload

---
+++ LOGINSYSTEM/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+
+    
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100&display=swap" rel="stylesheet">
+</head>
+<body>
+    
+<form action="userinfo.php" method="post">
+<div class="bigcard">
+    <div class="logocard">
+    </div>
+    <h1>Log in</h1>
+    <div class="lowerlogin">
+        <p>Welcome back! Please log in to your <br>account.</p>
+    </div>
+
+    <div class="username">
+        <p>Username</p>
+    </div>
+
+    <div class="usernamebox">
+        <input type="text" name="username">
+    </div>
+
+    <div class="password">
+        <p>Password</p>
+    </div>
+
+    <div class="passwordbox">
+        <input type="password" name="password">
+    </div>
+
+    <div class="checkbox">
+        <input type="checkbox">
+        <label for="checkbox">Remember me</label>
+    </div>
+
+    <input type="submit" class="button">
+
+    <div class="newuser">
+        <p>New User?</p>
+    </div>
+
+    <div class="newuserbutton">
+       <a href="weather.html">Sign up</a>
+
+    </div>
+
+    
+</div>
+</form>
+
+</body>
+</html>  
+
+<?php
+
+
+?>
---
+++ LOGINSYSTEM/userinfo.php
@@ -0,0 +1,10 @@
+<?php
+ $myusername= $_POST['username'];
+ $mypassword= $_POST['password'];
+
+ echo"Username: $myusername"."<br>";
+ echo"Password: $mypassword"."<br>";
+ 
+ 
+
+?>
